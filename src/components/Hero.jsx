26f4d4c8267cf146b5d5c6c993a377dd3e4c0ceb
Amr keshk <akeshk69@gmail.com>
2025-01-13 22:44:48 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,18 +10,18 @@ function Hero() {
   return (
     <div className='overflow-y-scroll'>
 
-      <div className='flex flex-col lg:flex-row lg:items-center w-full h-screen pt-[90px] px-8 sm:px-12 md:px-24 relative overflow-hidden'>
+      <div className='flex flex-col lg:flex-row lg:items-center w-full h-screen pt-28 px-8 sm:px-12 md:px-24 relative overflow-hidden'>
         <div className='layer absolute top-0 left-0 bottom-0 right-0 bg-secondaryColor bg-opacity-0 -z-10'></div>
         <div className='elements'>
           <img className='max-w-sm absolute -right-28 lg:-right-0 top-24 -z-10' src={sunVideo} alt="Sun animation representing innovation" />
           <img className='absolute w-24 lg:w-48 left-1/2 md:left-2/3 top-80 lg:left-32 lg:top-32 -z-20' src={paperRocket1} alt="Paper rocket flying towards the future" loading="lazy"/>
-          <img className='absolute w-20 lg:w-40 bottom-10 sm:bottom-32 right-4 lg:right-32 rotate-0' src={paperRocket} alt="Paper rocket flying towards the future" loading="lazy"/>
+          <img className='absolute w-20 lg:w-40 bottom-32 right-4 lg:right-32 rotate-0' src={paperRocket} alt="Paper rocket flying towards the future" loading="lazy"/>
         </div>
 
         <div className='flex-1 lg:flex-initial lg:w-4/6 xl:flex-1 flex justify-center flex-col'>
 
-          <h1 className="text-primaryTextColor font-bold uppercase">today <strong className='text-primaryColor'>programming</strong> is essential, <span className='block'>not optional</span></h1>
-          <ul className='text-primaryTextColor sm:my-8 '>
+          <h1 className="text-primaryTextColor font-bold uppercase">today, <strong className='text-primaryColor'>programming</strong> is essential, <span className='block'>not optional</span></h1>
+          <ul className='flex flex-col gap-2 text-primaryTextColor sm:my- '>
             <li className='flex items-center gap-4 font-medium'><i className="fa-solid fa-brain"></i>practical applications in every session.</li>
             <li className='flex items-center gap-4 font-medium'><i className="fa-solid fa-child-reaching"></i>Expert in practical children&apos;s education</li>
             <li className='flex items-center gap-4 font-medium'><i className="fa-solid fa-award"></i>Taught by a certified engineer</li>
@@ -31,7 +31,7 @@ function Hero() {
             <span className='block mb-6 text-xl'>Join us and start building the future</span>
             <button>
               <Link to="/contact" className='relative'>
-                <span className='uppercase bg-primaryColor p-2 my-4 sm:p-4 hover:bg-secondaryColor rounded-e-full hover:text-primaryColor transition-all duration-500'>start for free</span>
+                <span className='uppercase bg-primaryColor my-4 p-3 hover:bg-secondaryColor rounded-e-full hover:text-primaryColor transition-all duration-500'>start for free</span>
                 <img className='min-w-60 absolute left-1/2 top-1/2 -translate-y-1/2 -z-10' src={astro} alt="Astronaut in space representing future learning journey" />
               </Link>
             </button>
@@ -41,7 +41,7 @@ function Hero() {
 
 
         <div className='flex-1 lg:flex-initial lg:w-2/6 xl:flex-1'>
-          <img className='max-w-full mx-auto' src={bg} alt="Astronaut in space representing future learning journey" loading="lazy" />
+          <img className='max-w-sm mx-auto' src={bg} alt="Astronaut in space representing future learning journey" loading="lazy" />
         </div>
 
       </div>
